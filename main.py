cf2f4cf641099754df668bcbac88264a9ed2ebf8
Patryk Kozarski <Patryknerio2@gmail.com>
2019-04-01 11:26:17 +0200

    bubble sort

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+
+def swap(a, b):
+    temp = b
+    b = a
+    a = temp
+
+
+tab = range(100)
+for i in range(100):
+    tab[i] = random.randint(1, 100)
+    print tab[i],
+""" bubble sort: """
+for i in range(99):
+    for j in range(99):
+        if tab[j] > tab[j + 1]:
+            temp = tab[j]
+            tab[j] = tab[j + 1]
+            tab[j + 1] = temp
+print " "
+for i in tab:
+    print i,
