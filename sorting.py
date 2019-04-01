eb601f0fd2d9729925e6f81ebf72b9e8314aff34
Patryk Kozarski <Patryknerio2@gmail.com>
2019-04-01 11:30:32 +0200

    refresh

--- sorting.py
+++ sorting.py
@@ -11,6 +11,7 @@ tab = range(100)
 for i in range(100):
     tab[i] = random.randint(1, 100)
     print tab[i],
+
 """ bubble sort: """
 for i in range(99):
     for j in range(99):
@@ -18,6 +19,10 @@ for i in range(99):
             temp = tab[j]
             tab[j] = tab[j + 1]
             tab[j + 1] = temp
-print " "
+print "Bubble sort:"
+for i in tab:
+    print i,
+random.shuffle(tab)
+print ""
 for i in tab:
     print i,
